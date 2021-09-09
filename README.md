da811dc9d0f518c086030f272f7679d66a851e4a
Stefan Behrendt <behrendt@onexip.com>
2021-09-09 16:59:03 +0200

    implemented automatic translation and directory file reading

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ If you have an i18n library that uses JSON files for different languages, you ca
 properties.
 
 ## Use Cases
-If you have ever worked with internationalization (in short: _i18n_), you will have discovered a common problem: Inconsistency between different languages. Usually, you start developing your application in a default language (e. g. English), and at some point of time in the future, you take the English translation file, duplicate it and rename all properties' values by the correct strings of the new target language.
+If you have ever worked with internationalization (in short: _i18n_), you will have discovered a common problem: Inconsistency between different languages. Usually, you start developing your application in a default language (e. g. English) and at some point of time in the future, you take the English translation file, duplicate it and rename all properties' values by the correct strings of the new target language.
 
 However, if your app is continuously developed, new language-specific strings will be introduced and some may be deleted. And now you are in trouble: If you don't want to maintain each and every i18n property for all languages at all times, sooner or later you'll have missing strings in some languages. To avoid this problem, many i18n libraries automatically replace missing strings by those defined in a default language (e. g. English).
 
@@ -13,15 +13,38 @@ Using fallback strings is technically a simple solution, but it causes confusion
 
 ## Features
 - merge translation strings from different language files
-- several merge strategies (combine, reduce, add properties etc.)
-- write a custom string to inexistent properties
-- automatically translate strings using 
+- several merge strategies (combine, reduce, language-specific)
+- write a custom string placeholder to inexistent properties
+- automatically translate strings using [LibreTranslate](https://libretranslate.com/)
 
 ## Usage
-Use the following command to sync all JSON properties of different language files:
+Execute the following command to sync all JSON properties of different language files:
 
 ```
 node i18n-sync.js <i18n-directory>
 ```
 
 Replace `<i18n-directory>` with the directory name where your language definitions are saved.
+
+You can configure the way _i18n-sync_ works using command line parameters. For instance, if you want to merge your language files using the _reduce_ strategy, execute the following:
+
+```
+node i18n-sync.js -r
+```
+
+A list of all available configuration options can be found in the following section.
+
+### Configuration Options
+| Option                   | Command Line Parameter | Explanation  |
+| ------------------------ | ---------------------- | ------------ |
+| Combine Strategy         | -c                     | Use all properties of all languages.             |
+| Reduce Strategy          | -r                     | Use only properties that exist in all languages. |
+| Fit To Language Strategy | -f <language-file>     | Use all properties of a specific language. Omit all properties of other languages that do not occur in the provided language. |
+| Todo Placeholder         | -t                     | Write a "To Do" placeholder string into empty properties. |
+| Property Placeholder     | -p                     | Write a placeholder string into empty properties that uses the name of the current property. |
+| Individual Placeholder   | -i <placeholder>       | Write the provided placeholder string into empty properties. |
+| Automatic Translation    | -a                     | Automatically translates empty property values. Uses the reference string of the first language where the property is provided. |
+| Output Directory Name    | -o <output-dir-name>   | Define a custom output directory name.
+
+## License
+This tool uses the [MIT license](https://opensource.org/licenses/MIT). You can freely use and modify this software. Further information can be found in the `LICENSE` file.
--- i18n-sync.js
+++ i18n-sync.js
@@ -1,43 +1,140 @@
 // import libraries
-import { readdir, readFile } from "fs";
+const fs = require("fs");
+const https = require("https");
 
 function readFiles(dirPath, onFileContent, onError) {
-  readdir(dirPath, (error, filenames) => {
+  fs.readdir(dirPath, (error, filenames) => {
     if (error) {
       onError(error);
       return;
     }
 
-    filenames.forEach((filename) => {
-      readFile(dirPath + filename, "utf-8", (error, content) => {
+    filenames.forEach((fileName) => {
+      fs.readFile(`${dirPath}/${fileName}`, "utf-8", (error, content) => {
         if (error) {
           onError(error);
           return;
         }
-        onFileContent(filename, content);
+        onFileContent(fileName, content);
       });
     });
   });
 }
 
+async function translateAutomatically(
+  translateString,
+  sourceLanguage,
+  targetLanguage
+) {
+  const data = JSON.stringify({
+    q: translateString,
+    source: sourceLanguage,
+    target: targetLanguage
+  });
+
+  const options = {
+    hostname: "translate.argosopentech.com",
+    port: 443,
+    path: "/translate",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length
+    }
+  };
+
+  const req = https.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on("data", (d) => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on("error", (error) => {
+    console.error(error);
+  });
+
+  req.write(data);
+  req.end();
+}
+
 function howToUse() {
-  console.log("How to use i18n-sync");
+  console.log("\x1b[35m\x1b[47m");
+  console.log(`
+  |   |                     |                                 _) _ |   _ )                                          
+  |   |   _ \\ \\ \\  \\   /    __|   _ \\     |   |   __|   _ \\    |   |   _ \\   __ \\           __|  |   |  __ \\    __| 
+  ___ |  (   | \\ \\  \\ /     |    (   |    |   | \\__ \\   __/    |   |  (   |  |   | _____| \\__ \\  |   |  |   |  (    
+ _|  _| \\___/   \\_/\\_/     \\__| \\___/    \\__,_| ____/ \\___|   _|  _| \\___/  _|  _|        ____/ \\__, | _|  _| \\___| 
+                                                                                                ____/               `);
+  console.log("\x1b[0m");
+
+  console.log("\033[1m\n☛ Usage\033[0m\n");
+  console.log(
+    "Execute the following command to sync all JSON properties of different language files:\n"
+  );
+  console.log(
+    "  \x1b[32m> \x1b[0m\x1b[33mnode i18n-sync.js <i18n-directory> [parameters]\x1b[0m\n"
+  );
+  console.log(
+    "Replace \x1b[33m<i18n-directory>\x1b[0m with the directory name where your language definitions are saved.\n"
+  );
+  console.log(
+    "You can configure the way \x1b[35mi18n-sync\x1b[0m works using (optional) command line parameters.\n\n"
+  );
+
+  console.log("\033[1m☛ Command line parameters\n\033[0m");
+  console.log(
+    "\x1b[33m  -c\x1b[0m                   ➤ Use all properties of all languages."
+  );
+  console.log(
+    "\x1b[33m  -r\x1b[0m                   ➤ Use only properties that exist in all languages."
+  );
+  console.log(
+    "\x1b[33m  -f <language-file>\x1b[0m   ➤ Use all properties of a specific language and omit all other languages' properties."
+  );
+  console.log(
+    '\x1b[33m  -t\x1b[0m                   ➤ Write a "To Do" placeholder string into empty properties.'
+  );
+  console.log(
+    "\x1b[33m  -p\x1b[0m                   ➤ Write a placeholder string into empty properties that uses the name of the current property."
+  );
+  console.log(
+    "\x1b[33m  -i <placeholder>\x1b[0m     ➤ Write the provided placeholder string into empty properties."
+  );
+  console.log(
+    "\x1b[33m  -a\x1b[0m                   ➤ Automatically translates empty property values using LibreTranslate."
+  );
+  console.log(
+    "\x1b[33m  -o <output-dir-name>\x1b[0m ➤ Define a custom output directory name.\n"
+  );
+
+  console.log(
+    "\x1b[4mYou can only provide one parameter of each set:\x1b[0m\n"
+  );
+  console.log(
+    "\x1b[32mSTRATEGY:\x1b[0m     EITHER  \x1b[33m-c\x1b[0m  OR  \x1b[33m-r\x1b[0m  OR  \x1b[33m-f <language-file>\x1b[0m"
+  );
+  console.log(
+    "\x1b[32mPLACEHOLDER:\x1b[0m  EITHER  \x1b[33m-t\x1b[0m  OR  \x1b[33m-p\x1b[0m  OR  \x1b[33m-i <placeholder>\x1b[0m\n"
+  );
 }
 
 // BEGIN I18N SYNCHRONIZATION
-let params = process.argv;
+let params = process.argv; // 1st param: "node"; 2nd param: "i18n-sync.js"
 
 // check if i18n directory is provided
-if (params === undefined || params.length === 0) {
+if (params === undefined || params.length < 3) {
   howToUse();
   process.exit();
 }
 
-let dirPath = params[0];
+let dirPath = params[2];
 let data = {};
 readFiles(
-  dirPath,
+  `./${dirPath}`,
   (fileName, content) => {
+    console.log("CONTENT:", content);
     data[fileName] = content;
   },
   (error) => {
@@ -46,9 +143,29 @@ readFiles(
 );
 
 // DEBUG: log all file contents
-console.log("FILE CONTENTS", data);
+// console.log("FILE CONTENTS", data);
 
-// check if output directory already exists - if not, create it
-if (!fs.existsSync("output")) {
-  fs.mkdirSync("output");
+// check if output directory already exists
+if (!fs.existsSync("./output")) {
+  // output directory does not exist, so create it
+  fs.mkdirSync("./output");
+} else {
+  // check for old files and remove them
+  readFiles(
+    "./output",
+    (fileName, content) => {
+      fs.rmSync("./output/fileName", { force: true });
+    },
+    (error) => {
+      throw error;
+    }
+  );
 }
+
+// translate automatically
+// TODO: only trigger if command line parameter is set
+// translateAutomatically(
+//   "The quick brown fox jumps over the lazy cat",
+//   "en",
+//   "de"
+// );
